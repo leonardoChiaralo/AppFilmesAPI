6c47f33fc9fd30691963c18eed058685c5776c72
Leonardo Chiaralo <leonardo.chiaralo@db1.com.br>
2024-01-18 09:59:54 -0300

    Adicionar arquivos de projeto.

---
+++ AppFilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,42 @@
+﻿using AppFilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppFilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new();
+    private static int id = 0;
+
+    [HttpPost]
+    public IActionResult RegistrarFilme([FromBody] Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+        return CreatedAtAction(nameof(ExibirFilmePorId), new { id = filme.Id }, filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> ExibirFilmes()
+    {
+        return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult ExibirFilmePorId(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
+    }
+
+    [HttpGet("titulo/{titulo}")]
+    public IActionResult ExibirFilmePorTitulo(string titulo)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Titulo == titulo);
+        if (filme == null) return NotFound();
+        return Ok(filme);
+    }
+}
---
+++ AppFilmesAPI/Data/FilmeContext.cs
@@ -0,0 +1,13 @@
+﻿using AppFilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppFilmesAPI.Data;
+
+public class FilmeContext : DbContext
+{
+    public FilmeContext(DbContextOptions<FilmeContext> opts) : base(opts)
+    {
+    }
+
+    public DbSet<Filme> Filmes { get; set; }
+}
