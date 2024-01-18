2f0f9ef10feb1c577eeb59ff57c65faf53628b7e
Leonardo Chiaralo <leonardo.chiaralo@db1.com.br>
2024-01-18 10:52:35 -0300

    utilizando banco de dados

--- AppFilmesAPI/Controllers/FilmeController.cs
+++ AppFilmesAPI/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using AppFilmesAPI.Models;
+﻿using AppFilmesAPI.Data;
+using AppFilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppFilmesAPI.Controllers;
@@ -7,27 +8,37 @@ namespace AppFilmesAPI.Controllers;
 [Route("[controller]")]
 public class FilmeController : ControllerBase
 {
-    private static List<Filme> filmes = new();
-    private static int id = 0;
+   private FilmeContext _context;
+
+    public FilmeController(FilmeContext context)
+    {
+        _context = context;
+    }
 
     [HttpPost]
     public IActionResult RegistrarFilme([FromBody] Filme filme)
     {
-        filme.Id = id++;
-        filmes.Add(filme);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
         return CreatedAtAction(nameof(ExibirFilmePorId), new { id = filme.Id }, filme);
     }
 
     [HttpGet]
     public IEnumerable<Filme> ExibirFilmes()
     {
-        return filmes;
+        return _context.Filmes;
+    }
+
+    [HttpGet("paginados")]
+    public IEnumerable<Filme> ExibirFilmesPaginados([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return _context.Filmes.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
     public IActionResult ExibirFilmePorId(int id)
     {
-        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
         if (filme == null) return NotFound();
         return Ok(filme);
     }
@@ -35,7 +46,7 @@ public class FilmeController : ControllerBase
     [HttpGet("titulo/{titulo}")]
     public IActionResult ExibirFilmePorTitulo(string titulo)
     {
-        var filme = filmes.FirstOrDefault(filme => filme.Titulo == titulo);
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Titulo == titulo);
         if (filme == null) return NotFound();
         return Ok(filme);
     }
