53e6f0d60da495bb549fcff72bc543c60c9a96a2
Leonardo Chiaralo <leonardo.chiaralo@db1.com.br>
2024-02-05 16:26:52 -0300

    merge

--- AppFilmesAPI/Models/Artista.cs
+++ AppFilmesAPI/Models/Artista.cs
@@ -1,16 +1,14 @@
-﻿namespace AppFilmesAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppFilmesAPI.Models;
 
 public class Artista
 {
     public int Id { get; set; }
-    
-    [Required(ErrorMessage = "O nome do artísta é obrigatório.")]
     public string? Nome { get; set; }
 
-    [Required(ErrorMessage = "A data de nascimento do artísta é obrigatória.")]
     [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     [DataType(DataType.Date)]
-    public string? Nome { get; set; }
     public DateTime DataNascimento { get; set; }
     public int Idade { get; set; }
 }
